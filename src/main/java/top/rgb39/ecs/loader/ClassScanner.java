17c74befaa00ce3ba169e2d12750f85419b74745
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2024-03-20 01:40:14 +0800

    Update ClassScanner.java

--- src/main/java/top/rgb39/ecs/loader/ClassScanner.java
+++ src/main/java/top/rgb39/ecs/loader/ClassScanner.java
@@ -33,7 +33,7 @@ public class ClassScanner implements Scanner {
                             .replace(".class", "")
                             .replace("/", ".");
 
-                        Logger.ECS.info("Loading class %s -> %s", root, className);
+                        Logger.ECS.err("Loading class %s -> %s", root, className);
 
                         try {
                             Class<?> clz = cl.loadClass(className);
