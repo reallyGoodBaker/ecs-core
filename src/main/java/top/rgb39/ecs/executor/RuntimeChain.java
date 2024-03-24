3844029ba797914d0a4e294b52ceb8ce69f47f82
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2024-03-24 22:34:47 +0800

    Update RuntimeChain.java

--- src/main/java/top/rgb39/ecs/executor/RuntimeChain.java
+++ src/main/java/top/rgb39/ecs/executor/RuntimeChain.java
@@ -2,8 +2,6 @@ package top.rgb39.ecs.executor;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-
 import top.rgb39.ecs.arch.App;
 import top.rgb39.ecs.util.Logger;
 import top.rgb39.ecs.util.Logger.FontColors;
@@ -32,28 +30,19 @@ public class RuntimeChain {
         return (String[]) this.sysChains.keySet().toArray(new String[0]);
     }
 
-    public CompletableFuture<Void> scheduleOnce(App app) {
+    public void scheduleOnce(App app) {
         Logger.info("tick",FontColors.CYAN,  "schedule start");
-        CompletableFuture<?> ftr = CompletableFuture.runAsync(() -> {});
-        CompletableFuture<Void> future = _scheduleOnce(getScheduleSequence(), 0, ftr, app).thenRun(() -> {
-            Logger.info("tick",FontColors.YELLOW,  "schedule end");
-        });
-        return future;
-    }
-
-    private CompletableFuture<Void> _scheduleOnce(String[] scheduleSeq, int index, CompletableFuture<?> lastFuture, App app) {
-        return lastFuture.thenRun(() -> {
+        
+        int index = 0;
+        String[] scheduleSeq = getScheduleSequence();
+        while (index < scheduleSeq.length) {
             currentSchedule = scheduleSeq[index];
-            if (index < this.sysChains.size()) {
-                SystemChain sysChain = this.sysChains.get(scheduleSeq[index]);
-                CompletableFuture<Void> ftr = sysChain.run(app);
-                ftr.thenRun(() -> {
-                    _scheduleOnce(scheduleSeq, index + 1, ftr, app);
-                });
-            } else {
-                lastFuture.complete(null);
-            }
-        });
+            SystemChain sysChain = this.sysChains.get(scheduleSeq[index]);
+            sysChain.run(app);
+            index++;
+        }
+
+        Logger.info("tick",FontColors.YELLOW,  "schedule end");
     }
 
     public String current() {
