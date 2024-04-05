9ef5292beb1d66d3c0d4395f466c4978ba34ab30
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2024-04-06 02:13:26 +0800

    Update ClassScanner.java

--- src/main/java/top/rgb39/ecs/loader/ClassScanner.java
+++ src/main/java/top/rgb39/ecs/loader/ClassScanner.java
@@ -1,7 +1,9 @@
 package top.rgb39.ecs.loader;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.stream.Stream;
 
 import top.rgb39.ecs.util.Logger;
 
@@ -11,21 +13,34 @@ public class ClassScanner implements Scanner {
 
     static ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
-    public static void scan(String name) throws Exception {
-        Enumeration<URL> enums = cl.getResources(name);
+    public static void scan(String name) {
+        Enumeration<URL> enums;
+        try {
+            enums = cl.getResources(name);
+        } catch (IOException e) {
+            return;
+        }
 
         while (enums.hasMoreElements()) {
             URL url = enums.nextElement();
 
             if ("jar".equals(url.getProtocol())) {
-                JarScanner.scan(url);
+                try {
+                    JarScanner.scan(url);
+                } catch(Exception ignored) {}
                 continue;
             }
 
             if ("file".equals(url.getProtocol())) {
                 String path = url.getPath().substring(1);
-                Files.walk(Path.of(path))
-                    .filter(Files::isRegularFile)
+                Stream<Path> files;
+                try {
+                    files = Files.walk(Path.of(path));
+                } catch (IOException e) {
+                    continue;
+                }
+
+                files.filter(Files::isRegularFile)
                     .forEach(file -> {
                         String className = file.toString()
                             .replace("\\", "/");
@@ -48,7 +63,10 @@ public class ClassScanner implements Scanner {
                             Logger.ECS.e("%s", e);
                         }
                     });
+
+                files.close();
             }
+        
         }
     }
 
