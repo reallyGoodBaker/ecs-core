e93687ad2c612ca0b9f22cb2e57b928100f1a107
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2024-05-17 10:51:35 +0800

    fix

--- src/main/java/top/rgb39/ecs/loader/JarScanner.java
+++ src/main/java/top/rgb39/ecs/loader/JarScanner.java
@@ -3,29 +3,25 @@ package top.rgb39.ecs.loader;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
 import top.rgb39.ecs.util.Logger;
 
 public class JarScanner implements Scanner {
     public static void scan(URL jarFileURL) throws Exception {
-        URLClassLoader ucl = new URLClassLoader(new URL[]{jarFileURL});
-        JarURLConnection urlConnection = (JarURLConnection) jarFileURL.openConnection();
-        JarFile jarFile = urlConnection.getJarFile();
-        Enumeration<JarEntry> jarEntryEnumeration = jarFile.entries();
+        var ucl = new URLClassLoader(new URL[]{jarFileURL});
+        var urlConnection = (JarURLConnection) jarFileURL.openConnection();
+        var jarFile = urlConnection.getJarFile();
+        var jarEntryEnumeration = jarFile.entries();
         while (jarEntryEnumeration.hasMoreElements()) {
-            JarEntry jarEntry = jarEntryEnumeration.nextElement();
-            String jarEntryName = jarEntry.getName();
+            var jarEntry = jarEntryEnumeration.nextElement();
+            var jarEntryName = jarEntry.getName();
             if (!jarEntry.isDirectory() && jarEntryName.endsWith(".class") && !jarEntryName.contains("$") && !jarEntryName.contains("META-INF") && !jarEntryName.contains("module-info")) {
                 try {
                     var className = jarEntryName.replace("/", ".").replace(".class", "");
                     Logger.ECS.i("Loading class %s".formatted(className));
-                    Class<?> cls = ucl.loadClass(className);
+                    var cls = ucl.loadClass(className);
                     classes.put(cls.getName(), cls);
                 } catch (Exception e) {
-                    Logger.ECS.e("JarScanner: %s", e);
+                    Logger.ECS.e("JarScanner: %s".formatted(e));
                 }
             }
         }
